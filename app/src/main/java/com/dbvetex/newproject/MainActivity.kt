89e854790d4ea71e9185ffd35670639c5c81965d
shubham patel <dbvertextester@gmail.com>
2023-10-16 19:16:51 +0530

    Initial commit

---
+++ app/src/main/java/com/dbvetex/newproject/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.dbvetex.newproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
